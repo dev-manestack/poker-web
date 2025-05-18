8ff021c223b515378944a873c9194521739b0dc2
Khosbilegt Bilegsaikhan <archerdoc13@gmail.com>
2025-05-19 02:50:33 +0800

    added audios

--- src/assets/sounds/index.ts
+++ src/assets/sounds/index.ts
@@ -1,4 +1,7 @@
 import FlipCardAudio from "./flip-card.mp3";
 import DealCardAudio from "./deal-card.mp3";
+import ChipAudio from "./chip.mp3";
+import AllInAudio from "./all-in.mp3";
+import ShuffleAudio from "./shuffle.mp3";
 
-export { FlipCardAudio, DealCardAudio };
+export { FlipCardAudio, DealCardAudio, ChipAudio, AllInAudio, ShuffleAudio };
